3ecfa9b83678d50c22397189b09eb250c62304f2
Patrick J Volkerding <volkerdi@slackware.com>
2024-11-10 20:38:31 +0000

    Sun Nov 10 20:38:31 UTC 2024
    l/imagemagick-7.1.1_40-x86_64-1.txz:  Upgraded.
    l/python-hatchling-1.26.1-x86_64-1.txz:  Upgraded.
    l/python-zipp-3.21.0-x86_64-1.txz:  Upgraded.
    l/qt6-6.8.0_20241001_0c20a491-x86_64-1.txz:  Upgraded.
    n/c-ares-1.34.3-x86_64-1.txz:  Upgraded.
    n/netatalk-4.0.5-x86_64-1.txz:  Upgraded.
    x/xtrans-1.5.2-noarch-1.txz:  Upgraded.
    y/bsd-games-2.17-x86_64-6.txz:  Rebuilt.
      /etc/profile.d/bsd-games-login-fortune.{csh,sh}: suppress fortune output if
      $HOME/.hushlogin exists. Thanks to GazL.

--- source/y/bsd-games/bsd-games-login-fortune.csh
+++ source/y/bsd-games/bsd-games-login-fortune.csh
@@ -1,6 +1,8 @@
 #!/bin/csh
 # Print a fortune cookie for login shells:
 
-if ( { tty --silent } ) then >& /dev/null
+# Only output a fortune if $HOME/.hushlogin does not exist,
+# and this is an interactive shell:
+if ( ( ! -e $HOME/.hushlogin ) && { tty --silent } ) then >& /dev/null
   echo "" ; fortune fortunes fortunes2 linuxcookie ; echo ""
 endif
--- source/y/bsd-games/bsd-games-login-fortune.sh
+++ source/y/bsd-games/bsd-games-login-fortune.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-# Print a fortune cookie for interactive shells:
+# Print a fortune cookie for login shells:
 
-case $- in
-*i* )  # We're interactive
-  echo
-  fortune fortunes fortunes2 linuxcookie
-  echo
-  ;;
-esac
-
+# Only output a fortune if $HOME/.hushlogin does not exist:
+if [ ! -e $HOME/.hushlogin ]; then
+  # Only output a fortune on interactive shells:
+  case $- in
+  *i* )  # We're interactive
+    echo
+    fortune fortunes fortunes2 linuxcookie
+    echo
+    ;;
+  esac
+fi
